f1537805feeaf4c232c87b954abadd4f44a2e3a2
patriciosandor <64619046+patriciosandor@users.noreply.github.com>
2020-09-11 22:29:56 -0300

    Se completo hasta POLLO

--- js/archivo.js
+++ js/archivo.js
@@ -1,21 +1,39 @@
-var listadoDeNombre = []
-var listaDeMails= []
-var listaDeContraseñas = []
+
+const nuevoUsuario = {
+    listadoDeNombre : [],
+    listaDeMails : [],
+    listaDeContraseñas : []
+}
+
+
+// var listadoDeNombre = []
+// var listaDeMails= []
+// var listaDeContraseñas = []
 // -----------------------------------------------------------------
                             //BOTON QUIERO
 // -----------------------------------------------------------------
 function botonCompras(){
     usuario = prompt ('Ingresar con tu Mail')
-    for(i=0; i < listaDeMails.length; i++){
-        if (usuario == listaDeMails[i])
+    for(i=0; i < nuevoUsuario.listaDeMails.length; i++){
+        if (usuario == nuevoUsuario.listaDeMails[i])
         var nombre = i
     }
-    if(listaDeMails.includes(usuario) == true){
+
+    if(nuevoUsuario.listaDeMails.includes(usuario) == true){
         password = prompt ('Usted ya es un USUARIO Registrado introduzca su Contraseña.')
-        alert ('Bienvenido ' +listadoDeNombre[nombre] + ' ya esta todo listo para realizar su compra.' )
+            // alert ('Bienvenido ' + listadoDeNombre[nombre] + ' ya esta todo listo para realizar su compra.' )
+        
+        if((nuevoUsuario.listaDeMails.includes(usuario)) && (nuevoUsuario.listaDeContraseñas.includes(password))){
+            alert ('Bienvenido ' + nuevoUsuario.listadoDeNombre[nombre] + ' ya esta todo listo para realizar su compra.' )
+        }
+        
+        else{
+            alert('Tu contraseña es ERRONEA')
+        }
+
     } else{
         alert ('PARA ESTO, USTED SE TIENE QUE REGISTRAR, DIRIJASE AL CARRITO PARA COMPLETAR LOS DATOS.') 
-    }      
+    }
 }
 function buscarCuenta(){
     var lookUsuario = prompt ('Si no sos Usuario escriba "Nuevo" y si ya tienes usuario escriba "Usuario".')
@@ -24,9 +42,9 @@ function buscarCuenta(){
             nombreApellido = prompt ('Ingrese su Nombre y Apellido.')
             nuevoMail = prompt ('Le solicitamos que ingrese el Nuevo Mail.')
             nuevaContraseña = prompt ('Ingrese una nueva contraseña.')
-            listaDeMails.push(nuevoMail)
-            listaDeContraseñas.push(nuevaContraseña)
-            listadoDeNombre.push(nombreApellido)
+            nuevoUsuario.listaDeMails.push(nuevoMail)
+            nuevoUsuario.listaDeContraseñas.push(nuevaContraseña)
+            nuevoUsuario.listadoDeNombre.push(nombreApellido)
             alert ('Bienvenido ' + nombreApellido + ' ' + 'Le estaremos mandando una mail confirmando su cuenta nueva.')
         }
         else if (usuNuevo === 'Usuario'){
@@ -39,6 +57,7 @@ function buscarCuenta(){
     signIn(lookUsuario)
 
 }
+
 
 // -----------------------------------------------------------------
                             //OBJETO PLATOS
@@ -79,6 +98,13 @@ var Menu3_1 = new Comida ('BONDIOLA DESM C/MIEL, CERVEZA Y PAPAS', '-$400-', 'ME
 var Menu3_2 = new Comida ('BBQ RIBS C/PURE DE BATATA', '-$450-', 'MENU CERDO', 'm3-p2')
 var Menu3_3 = new Comida ('RIBS JACK DANIELS C/ PAPAS ESPECIALES', '-$490-', 'MENU CERDO', 'm3-p3')
 
+// COMIDA MENU POLLO
+//-----------------------------------
+
+var Menu4_1 = new Comida ('BASTONES DE POLLO C/ SALSA BBQ X 6un', '-$250-', 'MENU POLLO', 'm4-p1')
+var Menu4_2 = new Comida ('POLLO AL DIJON C/ PURE DE CALABAZA', '-$300-', 'MENU POLLO', 'm4-p2')
+var Menu4_3 = new Comida ('POLLO AL VERDEO C/ PAPAS RÚSTICAS', '-$300-', 'MENU POLLO', 'm4-p3')
+
 // FUNCTION PIZZAS Y PASTAS
 //-----------------------------------
 
@@ -103,3 +129,10 @@ Menu2_5.buscadorDePlato()
 Menu3_1.buscadorDePlato()
 Menu3_2.buscadorDePlato()
 Menu3_3.buscadorDePlato()
+
+// FUNCTION POLLO
+//-----------------------------------
+
+Menu4_1.buscadorDePlato()
+Menu4_2.buscadorDePlato()
+Menu4_3.buscadorDePlato()
