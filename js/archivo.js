65aaead6c9ec3bbe2aea018cdd90e3a9c2f51659
patriciosandor <64619046+patriciosandor@users.noreply.github.com>
2020-09-13 16:56:56 -0300

    Se termino agregando todo el menu a la funcion carrito

--- js/archivo.js
+++ js/archivo.js
@@ -5,7 +5,6 @@ const nuevoUsuario = {
     listaDeContraseñas : []
 }
 
-
 // var listadoDeNombre = []
 // var listaDeMails= []
 // var listaDeContraseñas = []
@@ -63,48 +62,79 @@ function buscarCuenta(){
                             //OBJETO PLATOS
 // -----------------------------------------------------------------
 
-var Carrito = []
+var carrito = []
 
-function Comida (plato, precio, sector, codigo){
+function Comida (plato, precio, sector, codigo, cantidad){
     this.plato = plato
     this.precio = precio
     this.sector = sector
     this.codigo = codigo
-    this.buscadorDePlato = function(){
-        console.log(this)
+    this.cantidad = cantidad
+    this.buscadorDePlato = function (){
+        var resultado = carrito.find(producto => producto.plato == plato)
+        if (resultado){
+
+            resultado.cantidad++
+        }
+        else{
+            const productoCarrito = {
+                plato : plato,
+                precio : precio,
+                sector : sector,
+                codigo : codigo,
+                cantidad : 1
+    
+            }
+            carrito.push(productoCarrito)
+        }
     }
 }
 // COMIDA MENU PIZZAS Y PASTAS
 //-----------------------------------
-var Menu1_1 = new Comida ('PIZZA DE MUZZARELLA INDIVIDUALES X3', '-$380-', 'MENU PIZZAS Y PASTAS', 'm1-p1') 
-var Menu1_2 = new Comida ('TARTAS INDIVIDUALES', '-$210-', 'MENU PIZZAS Y PASTAS', 'm1-p2') 
-var Menu1_3 = new Comida ('ÑOQUIS DE PAPA A LA BOLOGNESA', '-$260-', 'MENU PIZZAS Y PASTAS', 'm1-p3') 
-var Menu1_4 = new Comida ('SORRENTINOS DE CALABAZA C/ SALSA ROSA', '-$260-', 'MENU PIZZAS Y PASTAS', 'm1-p4') 
-var Menu1_5 = new Comida ('SORRENTINOSDE RICOTA JYQ C/ FILETO', '-$260-', 'MENU PIZZAS Y PASTAS', 'm1-p5') 
+var Menu1_1 = new Comida ('PIZZA DE MUZZARELLA INDIVIDUALES X3', '-$380-', 'MENU PIZZAS Y PASTAS', 'm1-p1', 0) 
+var Menu1_2 = new Comida ('TARTAS INDIVIDUALES', '-$210-', 'MENU PIZZAS Y PASTAS', 'm1-p2', 0) 
+var Menu1_3 = new Comida ('ÑOQUIS DE PAPA A LA BOLOGNESA', '-$260-', 'MENU PIZZAS Y PASTAS', 'm1-p3', 0) 
+var Menu1_4 = new Comida ('SORRENTINOS DE CALABAZA C/ SALSA ROSA', '-$260-', 'MENU PIZZAS Y PASTAS', 'm1-p4', 0) 
+var Menu1_5 = new Comida ('SORRENTINOSDE RICOTA JYQ C/ FILETO', '-$260-', 'MENU PIZZAS Y PASTAS', 'm1-p5', 0) 
 
 // COMIDA MENU CARNE
 //-----------------------------------
 
-var Menu2_1 = new Comida ('MILANESA DE LOMO NAPOLITANA C/ PAPINES', '-$300-', 'MENU CARNE', 'm2-p1')
-var Menu2_2 = new Comida ('KID MILNESA C/PURÉ', '-$260-', 'MENU CARNE', 'm2-p2')  
-var Menu2_3 = new Comida ('LOMO STROGONOFF C/ ARROZ', '-$370-', 'MENU CARNE', 'm2-p3')
-var Menu2_4 = new Comida ('LOMO AL CHAMPIGNON C/ ARROZ AMARILLO', '-$390-', 'MENU CARNE', 'm2-p4')
-var Menu2_5 = new Comida ('CARNE TIERNIZADA C/ PURE DE ZANAHORIA Y BATATA', '-$360-', 'MENU CARNE', 'm2-p5')  
+var Menu2_1 = new Comida ('MILANESA DE LOMO NAPOLITANA C/ PAPINES', '-$300-', 'MENU CARNE', 'm2-p1', 0)
+var Menu2_2 = new Comida ('KID MILNESA C/PURÉ', '-$260-', 'MENU CARNE', 'm2-p2', 0)  
+var Menu2_3 = new Comida ('LOMO STROGONOFF C/ ARROZ', '-$370-', 'MENU CARNE', 'm2-p3', 0)
+var Menu2_4 = new Comida ('LOMO AL CHAMPIGNON C/ ARROZ AMARILLO', '-$390-', 'MENU CARNE', 'm2-p4', 0)
+var Menu2_5 = new Comida ('CARNE TIERNIZADA C/ PURE DE ZANAHORIA Y BATATA', '-$360-', 'MENU CARNE', 'm2-p5', 0)  
 
 // COMIDA MENU CERDO
 //-----------------------------------
 
-var Menu3_1 = new Comida ('BONDIOLA DESM C/MIEL, CERVEZA Y PAPAS', '-$400-', 'MENU CERDO', 'm3-p1')
-var Menu3_2 = new Comida ('BBQ RIBS C/PURE DE BATATA', '-$450-', 'MENU CERDO', 'm3-p2')
-var Menu3_3 = new Comida ('RIBS JACK DANIELS C/ PAPAS ESPECIALES', '-$490-', 'MENU CERDO', 'm3-p3')
+var Menu3_1 = new Comida ('BONDIOLA DESM C/MIEL, CERVEZA Y PAPAS', '-$400-', 'MENU CERDO', 'm3-p1', 0)
+var Menu3_2 = new Comida ('BBQ RIBS C/PURE DE BATATA', '-$450-', 'MENU CERDO', 'm3-p2', 0)
+var Menu3_3 = new Comida ('RIBS JACK DANIELS C/ PAPAS ESPECIALES', '-$490-', 'MENU CERDO', 'm3-p3', 0)
 
 // COMIDA MENU POLLO
 //-----------------------------------
 
-var Menu4_1 = new Comida ('BASTONES DE POLLO C/ SALSA BBQ X 6un', '-$250-', 'MENU POLLO', 'm4-p1')
-var Menu4_2 = new Comida ('POLLO AL DIJON C/ PURE DE CALABAZA', '-$300-', 'MENU POLLO', 'm4-p2')
-var Menu4_3 = new Comida ('POLLO AL VERDEO C/ PAPAS RÚSTICAS', '-$300-', 'MENU POLLO', 'm4-p3')
+var Menu4_1 = new Comida ('BASTONES DE POLLO C/ SALSA BBQ X 6un', '-$250-', 'MENU POLLO', 'm4-p1', 0)
+var Menu4_2 = new Comida ('POLLO AL DIJON C/ PURE DE CALABAZA', '-$300-', 'MENU POLLO', 'm4-p2', 0)
+var Menu4_3 = new Comida ('POLLO AL VERDEO C/ PAPAS RÚSTICAS', '-$300-', 'MENU POLLO', 'm4-p3', 0)
 
+// COMIDA MENU ESTACION 
+//-----------------------------------
+
+var Menu5_1 = new Comida ('GUISO DE LENTEJA CASERO', '-$350-', 'MENU ESTACION', 'm5-p1', 0)
+var Menu5_2 = new Comida ('LOCRO CASERO', '-$350-', 'MENU ESTACION', 'm5-p2', 0)
+var Menu5_3 = new Comida ('TORTILLA DE PAPA XL', '-$300-', 'MENU ESTACION', 'm5-p3', 0)
+var Menu5_4 = new Comida ('RISOTO DE MARISCOS', '-$400-', 'MENU ESTACION', 'm5-p4', 0)
+
+// COMIDA MENU SALUDABLE
+//-----------------------------------
+
+var Menu6_1 = new Comida ('TARTA INTEGRAL DE VEGETALES', '-$260-', 'MENU SALUDABLE', 'm6-p1', 0)
+var Menu6_2 = new Comida ('TARTA INTEGRAL CAPRESSE', '-$260-', 'MENU SALUDABLE', 'm6-p2', 0)
+var Menu6_3 = new Comida ('WOK VEGETALES Y POLLO', '-$320-', 'MENU SALUDABLE', 'm6-p3', 0)
+var Menu6_4 = new Comida ('PECHUGA AL VACIO C/ ARROZ YAMANI Y VEGETALES', '-$350-', 'MENU SALUDABLE', 'm6-p4', 0)
 // FUNCTION PIZZAS Y PASTAS
 //-----------------------------------
 
@@ -136,3 +166,38 @@ Menu3_3.buscadorDePlato()
 Menu4_1.buscadorDePlato()
 Menu4_2.buscadorDePlato()
 Menu4_3.buscadorDePlato()
+
+// FUNCTION ESTACION
+//-----------------------------------
+
+Menu5_1.buscadorDePlato()
+Menu5_2.buscadorDePlato()
+Menu5_3.buscadorDePlato()
+Menu5_4.buscadorDePlato()
+
+
+// FUNCTION SALUDABLE
+//-----------------------------------
+
+Menu6_1.buscadorDePlato()
+Menu6_2.buscadorDePlato()
+Menu6_3.buscadorDePlato()
+Menu6_4.buscadorDePlato()
+
+// function buscadorDePlato (plato, precio, sector, codigo, cantidad){
+//     var resultado = carrito.find(producto => produto.nombre == nombre)
+//     if (reultado){
+//         resultado.cant++
+//     }
+//     else{
+//         const productoCarrito = {
+//             plato : plato,
+//             precio : precio,
+//             sector : sector,
+//             codigo : codigo,
+//             cantidad : 1
+
+//         }
+        //carrito.push(productoCarrito)
+//     }
+// }
