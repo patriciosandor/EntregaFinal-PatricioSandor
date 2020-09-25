0fcca8d5b06277963a5973897d2c6e94e9f75993
patriciosandor <64619046+patriciosandor@users.noreply.github.com>
2020-09-25 18:01:17 -0300

    COMMIT CON LAS CORRECCIONES DE MAURO

---
+++ js/objeto.js
@@ -0,0 +1,215 @@
+//----------------------------------------
+//----------------------------------------
+        // BASE DE DATOS 
+//----------------------------------------
+//----------------------------------------
+
+var baseDatos=[
+
+    //----------------------------------------
+            // PIZZAS Y PASTAS
+    //----------------------------------------
+    
+            {
+            nombre: 'PIZZA DE MUZZARELLA INDIVIDUALES X3',
+            precio : 300,
+            codigo: 1.00,
+            img : 'fotos/menu1plato1.jpg',
+            cantidad : 1
+        },
+    
+        {
+            nombre: 'TARTAS INDIVIDUALES',
+            precio : 210,
+            codigo: 1.01,
+            img : 'fotos/menu1plato2.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'ÑOQUIS DE PAPA A LA BOLOGNESA',
+            precio : 260,
+            codigo: 1.02,
+            img : 'fotos/menu1plato3.jpg',
+            cantidad : 1
+        },
+    
+        {
+            nombre: 'SORRENTINOS DE CALABAZA C/ SALSA ROSA',
+            precio : 260,
+            codigo: 1.03,
+            img : 'fotos/menu1plato4.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'SORRENTINOSDE RICOTA JYQ C/ FILETO',
+            precio : 260,
+            codigo: 1.04,
+            img : 'fotos/menu1plato5.jpg',
+            cantidad : 1
+        },
+    
+    //----------------------------------------
+                // CARNE
+    //----------------------------------------
+    
+        {
+            nombre: 'MILANESA DE LOMO NAPOLITANA C/ PAPINES',
+            precio : 300,
+            codigo: 2.00,
+            img : 'fotos/menu2plato1.jpg',
+            cantidad : 1
+        },
+    
+        {
+            nombre: 'KID MILNESA C/PURÉ',
+            precio : 260,
+            codigo: 2.01,
+            img : 'fotos/menu2plato2.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'LOMO STROGONOFF C/ ARROZ',
+            precio : 370,
+            codigo: 2.02,
+            img : 'fotos/menu2plato3.jpg',
+            cantidad : 1
+        
+        },
+    
+        {
+            nombre: 'LOMO AL CHAMPIGNON C/ ARROZ AMARILLO',
+            precio : 390,
+            codigo: 2.03,
+            img : 'fotos/menu2plato4.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'CARNE TIERNIZADA C/ PURE DE ZANAHORIA Y BATATA',
+            precio : 360,
+            codigo: 2.04,
+            img : 'fotos/menu2plato5.jpg',
+            cantidad : 1
+        },
+    
+    //----------------------------------------
+                // CERDO
+    //----------------------------------------
+    
+        {
+            nombre: 'BONDIOLA DESM C/MIEL, CERVEZA Y PAPAS',
+            precio : 400,
+            codigo: 3.00,
+            img : 'fotos/menu3plato1.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'BBQ RIBS C/PURE DE BATATA',
+            precio : 450,
+            codigo: 3.01,
+            img : 'fotos/menu3plato2.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'RIBS JACK DANIELS C/ PAPAS ESPECIALES',
+            precio : 490,
+            codigo: 3.02,
+            img : 'fotos/menu3plato3.jpg',
+            cantidad : 1
+        },
+    
+    //----------------------------------------
+                // POLLO
+    //----------------------------------------
+    
+    
+        {
+            nombre: 'BASTONES DE POLLO C/ SALSA BBQ X 6un',
+            precio : 350,
+            codigo: 4.00,
+            img : 'fotos/menu4plato1.jpg',
+            cantidad : 1
+        },
+    
+        {
+            nombre: 'POLLO AL DIJON C/ PURE DE CALABAZA',
+            precio : 300,
+            codigo: 4.01,
+            img : 'fotos/menu4plato2.jpg',
+            cantidad : 1
+    
+        },
+        {
+            nombre: 'POLLO AL VERDEO C/ PAPAS RÚSTICAS',
+            precio : 400,
+            codigo: 4.02,
+            img : 'fotos/menu4plato3.jpg',
+            cantidad : 1
+        },
+    
+    //----------------------------------------
+                // ESTACION
+    //----------------------------------------
+    
+        {
+            nombre: 'GUISO DE LENTEJA CASERO',
+            precio : 350,
+            codigo: 5.00,
+            img : 'fotos/menu5plato1.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'LOCRO CASERO',
+            precio : 350,
+            codigo: 5.01,
+            img : 'fotos/menu5plato2.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'TORTILLA DE PAPA XL',
+            precio : 300,
+            codigo: 5.02,
+            img : 'fotos/menu5plato3.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'RISOTO DE MARISCOS',
+            precio : 400,
+            codigo: 5.03,
+            img : 'fotos/menu5plato4.jpg',
+            cantidad : 1
+        },
+    
+    //----------------------------------------
+                // SALUDABLE
+    //----------------------------------------
+    
+        {
+            nombre: 'TARTA INTEGRAL DE VEGETALES',
+            precio : 260,
+            codigo: 6.00,
+            img : 'fotos/menu6plato1.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'TARTA INTEGRAL CAPRESSE',
+            precio : 260,
+            codigo: 6.01,
+            img : 'fotos/menu6plato2.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'WOK VEGETALES Y POLLO',
+            precio : 320,
+            codigo: 6.02,
+            img : 'fotos/menu6plato3.jpg',
+            cantidad : 1
+        },
+        {
+            nombre: 'PECHUGA AL VACIO C/ ARROZ YAMANI Y VEGETALES',
+            precio : 350,
+            codigo: 6.03,
+            img : 'fotos/menu6plato4.jpg',
+            cantidad : 1
+        },
+    
+    ]
